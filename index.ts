cdc78b099ef0fd17f19bc809b68b83668b7ba483
Reaver86 <alexander.schuster@web.de>
2020-09-22 23:33:11 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { interval, of } from 'rxjs'; 
-import { map, toArray, scan, groupBy } from 'rxjs/operators';
+import { map, toArray, scan, groupBy, mergeMap, concatMap } from 'rxjs/operators';
 
 const source = interval(1000).pipe(
   map(nr => ({key: nr < 20 ? 'x' : 'y', value: nr})),
   groupBy(x => x.key),
-  mergeMap()
+  concatMap(group$ => group$.pipe(
+    map(obj => ({key: group$.key, value: obj}))
+  ))
   // scan((acc, curr) => acc + curr.value, 0)
 );
 
